5250cfc091812c36c65ce55e12a6a3088ad374e2
IkLaa <58468744+IkLaa@users.noreply.github.com>
2021-09-07 17:36:19 +0100

    Add files via upload

---
+++ CollaBIM/Assets/Cross Section Shader/Scripts/OnePlaneCuttingController2.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+//[ExecuteInEditMode]
+public class OnePlaneCuttingController2 : MonoBehaviour
+{
+
+    public GameObject plane;
+    Material mat;
+    public Vector3 normal;
+    public Vector3 position;
+    public Renderer rend;
+    // Use this for initialization
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        normal = plane.transform.TransformVector(new Vector3(0, 0, -1));
+        position = plane.transform.position;
+        UpdateShaderProperties();
+    }
+    void Update()
+    {
+        UpdateShaderProperties();
+    }
+
+    private void UpdateShaderProperties()
+    {
+        normal = plane.transform.TransformVector(new Vector3(0, 0, -1));
+        position = plane.transform.position;
+        rend.materials[0].SetVector("_PlaneNormal", normal);
+        rend.materials[1].SetVector("_PlaneNormal", normal);
+        rend.materials[0].SetVector("_PlanePosition", position);
+        rend.materials[1].SetVector("_PlanePosition", position);
+    }
+}
